0d5508e90feafe3e9a1a6c4ce693d0991d6a517f
OhmPatil <workpatil10@gmail.com>
2023-02-04 18:07:16 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useRef, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+// action="https://listmonk-production-ef6b.up.railway.app/subscription/form"
+
+function App() {
+  const emailRef = useRef();
+  const nameRef = useRef();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const response = await fetch(
+      "https://neofolks-server.up.railway.app/participants/",
+      {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({"name": nameRef.current.value, "email": emailRef.current.value})
+        // body: {"name": "new name", "email": "workpatil10@gmail.com"}
+
+      }
+    );
+    console.log(response)
+    console.log(nameRef.current.value)
+    console.log(emailRef.current.value)
+  }
+
+  async function getAll(){
+    const response = await fetch("https://neofolks-server.up.railway.app/participants", {mode: 'cors', method: "GET"})
+    const participants = await response.json()
+    console.log(participants)
+  }
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='Name' ref={nameRef}/>
+        <input type="text" placeholder="Email" ref={emailRef} />
+        <input type="submit" value="submit" />
+      </form>
+      {/* <button onClick={getAll}>get all</button> */}
+    </div>
+  );
+}
+
+export default App;
